2d7902c39faad37455b1217440485be15abd5440
catplunk <gojerk2@yahoo.com>
2020-12-01 13:16:04 -0500

    better solution

--- day1/src/main/kotlin/main.kt
+++ day1/src/main/kotlin/main.kt
@@ -2,30 +2,31 @@ fun main(args: Array<String>) {
     val inputStrings = object {}.javaClass.getResource("input.txt").readText(Charsets.UTF_8).split("\n")
     val input = inputStrings.mapNotNull { it.toIntOrNull() }
 
-    // find 2 numbers that when summed equals 2020
-    println("2 numbers:")
-    for(i in input.indices) {
-        var j = i + 1
-        while (j < input.size - 1) {
-            j++
-            if (input[i] + input [j] == 2020) {
-                println("${input[i]} * ${input[j]} = ${input[i] * input[j]}")
+    val pairSolution = pairs(input).filter { (a,b) -> a + b == 2020 }
+        .map { (a, b) -> a * b }
+        .first()
+
+    val tripleSolution = triples(input).filter { (a,b,c) -> a + b + c == 2020 }
+        .map { (a, b, c) -> a * b * c}
+        .first()
+
+    println("pair solution: $pairSolution\ttriple solution: $tripleSolution")
+}
+
+fun <T> pairs(input: List<T>): Sequence<Pair<T, T>> = sequence {
+        for (i in 0 until input.size - 1) {
+            for (j in i+1 until input.size) {
+                yield(input[i] to input[j])
             }
         }
     }
 
-    println("3 numbers:")
-    for (i in input.indices) {
-        var j = i + 1
-        while (j < input.size - 1){
-            var k = j + 1
-            while (k < input.size - 2) {
-                k++
-                if (input[i] + input [j] + input[k] == 2020) {
-                    println("${input[i]} * ${input[j]} * ${input[k]} = ${input[i] * input[j] * input[k]}")
-                }
+fun <T> triples(input: List<T>): Sequence<Triple<T,T,T>> = sequence {
+    for (i in 0 until input.size - 2) {
+        for (j in i + 1 until input.size - 1) {
+            for (k in j + 1 until input.size) {
+                yield(Triple(input[i], input[j], input[k]))
             }
-            j++
         }
     }
 }
